acb61cfeef58b80b6fbe871d06056754ea0c1acd
darkduck741 <124246256+darkduck741@users.noreply.github.com>
2023-02-07 00:27:42 +0100

    #2

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-function chageIcon(domImg,srcImage)
-    {
-        var img = new Image();
-        img.onload = function()
-        {
-            // Load completed
-            domImg.src = this.src;
-        };
-        img.src = srcImage;
-    }
+
 
 
